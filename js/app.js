27fcd82f9f74697ae01a58d1366afa3d39af78ff
nkong008e <nkong2020@gmail.comm>
2022-10-26 11:42:37 -0500

    updated files

--- js/app.js
+++ js/app.js
@@ -11,12 +11,37 @@ bodY.appendChild(wrapper);
 for ( let i = 0; i < 4; i++ ) {
 let divs = document.createElement("div");
 wrapper.appendChild(divs);
-divs.classList.add("WrapperDivs")
+divs.classList.add("wrapperDivs");
 }
-var color1 = "linear-gradient(135deg, #0B132B 0%, #3A506B 100%)";
-var color2 = "linear-gradient(330deg, #5BC0BE 0%, #3A506B 100%)";
-var color3 = "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
-var color4 = "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)";
-const colorsArray = ["color1", "color2","color3","color4"];
-console.log(colorsArray);
+let color1 = "linear-gradient(135deg, #0B132B 0%, #3A506B 100%)";
+let color2 = "linear-gradient(330deg, #5BC0BE 0%, #3A506B 100%)";
+let color3 = "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
+let color4 = "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)";
+const colorsArray = [color1,color2,color3,color4];
+//console.log(colorsArray);//
 
+let wrapper_Divs = document.getElementsByClassName('wrapperDivs');
+//console.log(Wrapper_Divs);
+
+let divOne = wrapper_Divs.item(0);
+let divTwo = wrapper_Divs.item(1);
+let divThree = wrapper_Divs.item(2);
+let divFour = wrapper_Divs.item(3);
+
+let allDivs = [divOne,divTwo,divThree,divFour];
+//console.log(allDivs);
+
+for (let i = 0; i< 4; i++) {
+    let myDiv = allDivs[i];
+    myDiv.style.backgroundImage = colorsArray[i];
+    let update= document.createElement('section');
+    myDiv.appendChild(update);
+    update.setAttribute("id", "ID" + [i]);
+}
+function Artist(artistName, album, label, albumImg, bio); {
+    this.artistName = artistName;
+    this.album = album;
+    this.label = label;
+    this.albumImg = albumImg;
+    this.bio = bio;
+}
